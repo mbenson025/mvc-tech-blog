a3563823398ebbe9e39a8746d07472c3362d0601
mbenson025 <mbenson025@gmail.com>
2022-07-24 20:13:17 -0500

    individual post by id

--- Controllers/homeRoutes.js
+++ Controllers/homeRoutes.js
@@ -32,6 +32,45 @@ router.get('/', async (req, res) => {
   }
 });
 
+//-------------individual posts--------------------
+router.get('/post/:id', (req, res) => res.render('post'));
+// router.get('/post/:id', async (req, res) => {
+//   try {
+//     const postData = await Post.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['username'],
+//         },
+//         // {
+//         //   model: Comment,
+//         //   attributes: [
+//         //     'id',
+//         //     'comment_content',
+//         //     'post_id',
+//         //     'user_id',
+//         //     // 'created_at',
+//         //   ],
+//         //   include: {
+//         //     model: User,
+//         //     attributes: ['username'],
+//         //   },
+//         // },
+//       ],
+//     });
+
+//     const post = postData.get({ plain: true });
+
+//     res.render('post', {
+//       ...post,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+//-------------individual posts--------------------
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   // if (req.session.logged_in) {
